3b5af701f433e8bdaeb77bd7a9c79e561dee1988
Filip Jirsák <filip@jirsak.org>
2024-05-06 22:49:09 +0200

    Aktualizace jaro 2024 – Java 21, Gradle 8.7

--- src/main/java/cz/czechitas/java2webapps/lekce5/entity/Gender.java
+++ src/main/java/cz/czechitas/java2webapps/lekce5/entity/Gender.java
@@ -1,17 +1,17 @@
 package cz.czechitas.java2webapps.lekce5.entity;
 
 public enum Gender {
-  Female("female"),
-  Male("male"),
-  ;
+    Female("female"),
+    Male("male"),
+    ;
 
-  private final String title;
+    private final String title;
 
-  Gender(String title) {
-    this.title = title;
-  }
+    Gender(String title) {
+        this.title = title;
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    public String getTitle() {
+        return title;
+    }
 }
--- src/main/java/cz/czechitas/java2webapps/lekce5/people/FamousPeopleController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce5/people/FamousPeopleController.java
@@ -1,40 +1,36 @@
 package cz.czechitas.java2webapps.lekce5.people;
 
 import cz.czechitas.java2webapps.lekce5.entity.Gender;
-import cz.czechitas.java2webapps.lekce5.entity.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.time.LocalDate;
 
 /**
  * @author Filip Jirsák
  */
 @Controller
 public class FamousPeopleController {
-  private final FamousPeopleService service;
+    private final FamousPeopleService service;
 
-  public FamousPeopleController(FamousPeopleService service) {
-    this.service = service;
-  }
+    public FamousPeopleController(FamousPeopleService service) {
+        this.service = service;
+    }
 
-  @GetMapping("/")
-  public ModelAndView list() {
-    ModelAndView result = new ModelAndView("index");
-    result.addObject("people", service.getAll());
-    result.addObject("gender", Gender.values());
-    return result;
-  }
+    @GetMapping("/")
+    public ModelAndView list() {
+        ModelAndView result = new ModelAndView("index");
+        result.addObject("people", service.getAll());
+        result.addObject("gender", Gender.values());
+        return result;
+    }
 
-  @GetMapping("/{id}")
-  public ModelAndView detail(@PathVariable int id) {
-    ModelAndView result = new ModelAndView("detail");
-    result.addObject("person", service.getById(id));
-    result.addObject("gender", Gender.values());
-    return result;
-  }
+    @GetMapping("/{id}")
+    public ModelAndView detail(@PathVariable int id) {
+        ModelAndView result = new ModelAndView("detail");
+        result.addObject("person", service.getById(id));
+        result.addObject("gender", Gender.values());
+        return result;
+    }
 
 }
